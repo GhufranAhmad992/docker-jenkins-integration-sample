092f1714bed6a930f56412394b967397a08ebe36
Imran Khan <ghufranahmad210@gmail.com>
2023-03-24 04:25:53 +0530

    Thirt Commit

--- src/main/java/com/abclearning/docker_jenkins_integration_sample/Test.java
+++ src/main/java/com/abclearning/docker_jenkins_integration_sample/Test.java
@@ -3,6 +3,7 @@ package com.abclearning.docker_jenkins_integration_sample;
 public class Test {
 	public static void main(String[] args) {
 		System.out.println("First change to check integration");
+		System.out.println("Integration Success!!");
 	}
 
 }
